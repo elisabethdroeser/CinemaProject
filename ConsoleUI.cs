d170b4d9feb0d43cb19aea2df6a255bcf00ba724
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2021-12-06 10:12:00 +0100

    IUI

---
+++ ConsoleUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CinemaProject
+{
+    public class ConsoleUI : IUI
+    {
+        public string GetStringInput()
+        {
+            return Console.ReadLine();
+        }
+        public void PrintString(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class MockUI : IUI
+    {
+        public string GetStringInput()
+        {
+            return "1";
+        }
+
+        public void PrintString(string message);
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace CinemaProject
 {
     class Program
     {
+        static IUI ui = new ConsoleUI();
         static void Main(string[] args)
         {
             //Display text on header
@@ -13,24 +14,47 @@ namespace CinemaProject
             //Ask the user to choose an option
             Console.WriteLine("0 - Exit");
             Console.WriteLine("1 - Customer");
-            Console.WriteLine("2 - Repeat Ten Times");
+            Console.WriteLine("2 - Repeat ten times");
             Console.WriteLine("3 - The third word");
+
+            do
+            {
+                ShowMainMenu();
+                UserInput();
+            } while (true);
         }
 
-        /*private static void UserInput()
+        private static void ShowMainMenu()
+        {
+            ui.PrintString("0 - Exit");
+            ui.PrintString("1 - Customer");
+            ui.PrintString("2 - Repeat ten times");
+            ui.PrintString("3 - The third word");
+        }
+
+        private static void UserInput()
         {
             //Different actions for the user to perform on menu
             var input = uint.GetStringInput();
             switch (input)
             {
-                case "0";
+                case "0":
                     //Ends the program
                     Environment.Exit(0);
-                    break; 
-                case "1"; 
-
-
+                    break;
+                case "1":
+                    ChooseCustomer();
+                    break;
+                case "2":
+                    RepeatInput();
+                    break;
+                case "3":
+                    ChooseThirdWord;
+                    break;
+                default:
+                    uint.PrintString("Wrong input");
+                    break;
             }
-        }*/
+        }
     }
 }
