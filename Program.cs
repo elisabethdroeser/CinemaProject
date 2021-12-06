54b4056c33f0df986c41907352006ff13f6c8fa4
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2021-12-06 09:32:07 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CinemaProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+
+            //Display text on header
+            Console.WriteLine("Cinema Le Xicon");
+            Console.WriteLine("You have reached our menu");
+            Console.WriteLine("Choose an option from the following list");
+            Console.WriteLine("\t0 - Exit");
+            Console.WriteLine("\t1 - Customer");
+            Console.WriteLine("\t0 - Repeat Ten Times");
+            Console.WriteLine("\t0 - The third word");
+        }
+
+        /*private static void UserInput()
+        {
+            //Different actions for the user to perform on menu
+            var input = uint.GetStringInput();
+            switch (input)
+            {
+                case "0";
+                    //Ends the program
+                    Environment.Exit(0);
+                    break; 
+                case "1"; 
+
+
+            }*/
+        }
+    }
+
+
+}
